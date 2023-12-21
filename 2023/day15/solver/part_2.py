b941cc9e1de6e7ddf62f404abb925a22fc4bbafb
Morten Hauge <morten.hauge97@gmail.com>
2023-12-21 18:57:45 +0100

    chore: Uploaded solution for Day 15 - Part 2

--- 2023/day15/solver/part_2.py
+++ 2023/day15/solver/part_2.py
@@ -1,5 +1,55 @@
 from solver import utils
+from functools import reduce
+from collections import defaultdict
+
+
+def HASH(instruction):
+    current_value = 0
+
+    for char in instruction:
+        current_value += ord(char)
+        current_value *= 17
+        current_value = current_value % 256
+
+    return current_value
 
 
 def solve(input_file: str):
-    lines = utils.read_lines(input_file)
+    instructions = utils.read_lines(input_file)
+    instructions = instructions.split(",")
+
+    labels = defaultdict(list)
+    lenses = defaultdict(list)
+
+    for instruction in instructions:
+        if "-" in instruction:
+            label = instruction[:-1]
+            box_number = HASH(label)
+
+            try:
+                index = labels[box_number].index(label)
+                labels[box_number].pop(index)
+                lenses[box_number].pop(index)
+            except ValueError:
+                pass
+        else:
+            label, focal_length = instruction.split("=")
+            box_number = HASH(label)
+
+            try:
+                index = labels[box_number].index(label)
+                lenses[box_number].pop(index)
+                lenses[box_number].insert(index, focal_length)
+            except ValueError:
+                labels[box_number].append(label)
+                lenses[box_number].append(focal_length)
+
+    focusing_powers = []
+
+    for box_number, box_labels in labels.items():
+        for slot, label in enumerate(box_labels):
+            focal_length = lenses[box_number][slot]
+            focusing_power = (box_number + 1) * (slot + 1) * int(focal_length)
+            focusing_powers.append(focusing_power)
+
+    return reduce(lambda x, y: x + y, focusing_powers)
--- 2023/day15/tests/test_solve.py
+++ 2023/day15/tests/test_solve.py
@@ -1,16 +1,16 @@
 from solver import solve_1, solve_2
 
 
-def test_part_1():
-    output = solve_1("./task_input/test_1.txt")
+# def test_part_1():
+#     output = solve_1("./task_input/test_1.txt")
 
-    if output is None:
-        return False
+#     if output is None:
+#         return False
 
-    assert output == 1320
+#     assert output == 1320
 
-    output_1 = solve_1("./task_input/input.txt")
-    print("\nPart 1 solution:", output_1)
+#     output_1 = solve_1("./task_input/input.txt")
+#     print("\nPart 1 solution:", output_1)
 
 
 def test_part_2():
@@ -19,7 +19,7 @@ def test_part_2():
     if output is None:
         return True
 
-    assert output == ...
+    assert output == 145
 
     output_2 = solve_2("./task_input/input.txt")
     print("\nPart 2 solution:", output_2)
